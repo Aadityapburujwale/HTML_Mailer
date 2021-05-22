e8dfc442a33ea1ae19152b877d96053631fa65bc
Aaditya P Burujwale <63345229+Aadityapburujwale@users.noreply.github.com>
2021-05-22 10:51:59 +0530

    README.md Added, HTML Preview Feature Added , Some UI Bug Fixed.

---
+++ README.md
@@ -0,0 +1,21 @@
+# HTML_Mailer
+With the help of 'HTML Mailer App' user can send Html Email from android device.
+I have used the #java mail api in the application to send an emails.
+
+<h2>Feature 👇</h2>
+<<<<<<< HEAD
+• With the help of this application user can send <b>html email</b> using android device.<br>
+• Preview of html email (User Can See preview How email will look like before sending an email).
+
+<h2>Upcoming Features👇 </h2>(New features in my mind 😅 that I will implement soon.)<br>
+=======
+With the help of this application user can send <b>html email</b> using android device.
+
+<h2>Upcoming Features👇 </h2>(New features in my mind 😅 that I will implement soon.)<br>
+• Preview of html email (User Can See preview How email will look like before sending an email)<br>
+>>>>>>> refs/remotes/origin/master
+• Remember Email Id & Password.<br>
+• Multimedia Support<br>
+
+
+[<h2>Click Here To Watch Application Demo</h2>](https://drive.google.com/file/d/1HIRHi_I6COUUnmzDfNuRcX0d655kyWdc/view?usp=sharing)
--- app/src/main/java/com/htmlmailer/MainActivity.java
+++ app/src/main/java/com/htmlmailer/MainActivity.java
@@ -1,20 +1,24 @@
 package com.htmlmailer;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
+import android.view.Window;
+import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
-import javax.mail.Session;
 import java.util.Properties;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Authenticator;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
@@ -22,104 +26,135 @@ import javax.mail.internet.MimeMessage;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText userName_editText;
+    EditText password_editText;
+    EditText recipient_Email_editText;
+    EditText title_editText;
+    EditText message_editText;
+    Button openGoogleSetting_button;
+    CheckBox done;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName_editText = findViewById(R.id.enter_email);
+        password_editText = findViewById(R.id.enter_password);
+        recipient_Email_editText = findViewById(R.id.enter_Remail);
+        title_editText = findViewById(R.id.enter_title);
+        message_editText = findViewById(R.id.enter_message);
+        openGoogleSetting_button = findViewById(R.id.open_setting_btn);
+        done = findViewById(R.id.done_checkBox);
+
+        openGoogleSetting_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openSetting = new Intent(Intent.ACTION_VIEW, Uri.parse("https://myaccount.google.com/lesssecureapps"));
+                startActivity(openSetting);
+            }
+        });
+
     }
 
     public void sendMail(View view) {
 
-        EditText userName_editText = findViewById(R.id.enter_email);
-        EditText password_editText = findViewById(R.id.enter_password);
-        EditText recipient_Email_editText = findViewById(R.id.enter_Remail);
-        EditText title_editText = findViewById(R.id.enter_title);
-        EditText message_editText = findViewById(R.id.enter_message);
-
         final String userName = userName_editText.getText().toString();
         final String password = password_editText.getText().toString();
-
         String recipient_Email = recipient_Email_editText.getText().toString();
         String title = title_editText.getText().toString();
         String message = message_editText.getText().toString();
 
-        if(anyOneEmpty(userName,password,message,title,recipient_Email))
-            sendEmail(userName,password,message,title,recipient_Email);
+        if (anyOneEmpty(userName, password, message, title, recipient_Email))
+            sendEmail(userName, password, message, title, recipient_Email);
     }
 
-    boolean anyOneEmpty(String userName,String password, String textMail , String title, String recipient_Email){
+    boolean anyOneEmpty(String userName, String password, String message, String title, String recipient_Email) {
 
-        EditText userName_editText = findViewById(R.id.enter_email);
-        EditText password_editText = findViewById(R.id.enter_password);
-        EditText recipient_Email_editText = findViewById(R.id.enter_Remail);
-        EditText title_editText = findViewById(R.id.enter_title);
-        EditText message_editText = findViewById(R.id.enter_message);
-        CheckBox done = findViewById(R.id.done_checkBox);
-
-        if(userName.matches("")){
+        if (userName.matches("")) {
             userName_editText.setError("Please Enter A Email Id");
-            Toast.makeText(getApplicationContext(),"Please Enter A Email Id",Toast.LENGTH_LONG).show();
             return false;
         }
-        if(password.matches("")){
+        if (password.matches("")) {
             password_editText.setError("Please Enter Password");
-            Toast.makeText(getApplicationContext(),"Please Enter Password",Toast.LENGTH_LONG).show();
             return false;
         }
-        if(textMail.matches("")){
+        if (message.matches("")) {
             message_editText.setError("Please Enter A Message you want to send (In Html)");
-            Toast.makeText(getApplicationContext(),"Please Enter A Message you want to send (In Html)",Toast.LENGTH_LONG).show();
             return false;
         }
-        if(title.matches("")){
+        if (title.matches("")) {
             title_editText.setError("Please Enter A Title/Subject Of Email");
-            Toast.makeText(getApplicationContext(),"Please Enter A Title/Subject Of Email",Toast.LENGTH_LONG).show();
             return false;
         }
-        if(recipient_Email.matches("")){
+        if (recipient_Email.matches("")) {
             recipient_Email_editText.setError("Please Enter A Recipient Email Id");
-            Toast.makeText(getApplicationContext(),"Please Enter A Recipient Email Id",Toast.LENGTH_LONG).show();
             return false;
         }
-        if(!done.isChecked()){
-            Toast.makeText(getApplicationContext(),"Please Turn On The Setting From Google Ac. and click On Done .",Toast.LENGTH_LONG).show();
+        if (!done.isChecked()) {
+            Toast.makeText(getApplicationContext(), "Please Turn On The Setting From Google Ac. and click On Done .", Toast.LENGTH_LONG).show();
             return false;
         }
 
         return true;
     }
 
-    void sendEmail(String userName,String password, String textMail , String title, String recipient_Email){
+
+    void sendEmail(String userName, String password, String textMail, String title, String recipient_Email) {
 
         Properties properties = new Properties();
-        properties.put("mail.smtp.auth","true");
-        properties.put("mail.smtp.starttls.enable","true");
-        properties.put("mail.smtp.host","smtp.gmail.com");
-        properties.put("mail.smtp.port","587");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
         Session session = javax.mail.Session.getInstance(properties,
-                new Authenticator(){
+                new Authenticator() {
                     @Override
-                    protected PasswordAuthentication getPasswordAuthentication(){
-                        return new PasswordAuthentication(userName,password);
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(userName, password);
                     }
                 });
-        try{
+        try {
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(userName));
-            message.setRecipients(MimeMessage.RecipientType.TO,InternetAddress.parse(recipient_Email));
+            message.setRecipients(MimeMessage.RecipientType.TO, InternetAddress.parse(recipient_Email));
             message.setSubject(title);
-            message.setContent(textMail,"text/html");
+            message.setContent(textMail, "text/html");
             Transport.send(message);
-            Toast.makeText(getApplicationContext(),"Email Send Successfully.",Toast.LENGTH_LONG).show();
-        }catch (MessagingException e){
-            Toast.makeText(getApplicationContext(),"Something Went Wrong.",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Email Send Successfully.", Toast.LENGTH_LONG).show();
+        } catch (MessagingException e) {
+            Toast.makeText(getApplicationContext(), "Something Went Wrong.", Toast.LENGTH_LONG).show();
         }
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
     }
 
-    public void openGoogleSetting(View view) {
-        Intent goToTheSetting = new Intent(Intent.ACTION_VIEW, Uri.parse("https://myaccount.google.com/lesssecureapps"));
-        startActivity(goToTheSetting);
+
+    // Show_HTML_Preview Button
+    public void show_html_preview(View view) {
+
+        Dialog htmlPreview = new Dialog(this);
+        htmlPreview.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        htmlPreview.setContentView(R.layout.html_viewer);
+        TextView close = htmlPreview.findViewById(R.id.close);
+        String message = message_editText.getText().toString();
+
+        if (message.length() > 0) {
+            WebView htmlView = htmlPreview.findViewById(R.id.webview);
+            htmlView.loadDataWithBaseURL(null, message, "text/html", "utf-8", null);
+            htmlPreview.show();
+        } else {
+            message_editText.setError("Please Enter A Message you want to send (In Html)");
+        }
+
+
+        // Close Button
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                htmlPreview.dismiss();
+            }
+        });
+
     }
 }
